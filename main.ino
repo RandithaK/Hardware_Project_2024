7211865a2237ff22de98219ec4aeab00143f15e0
RandithaK <bharkula3@gmail.com>
2024-02-06 01:36:20 +0530

    Added some more blank functions

--- main.ino
+++ main.ino
@@ -11,3 +11,15 @@ void loop() {
 void get_keypad_input(){
   // put code to get keypad input
 }
+
+void dispense(){
+  // put code to dispense gram
+}
+
+void error(){
+  // activate buzzer to give cricket alarm
+}
+
+void mode_switch(){
+  
+}
