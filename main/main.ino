b44a07863debbb44dcfa86ac8f54361c24e5cbfa
RandithaK <bharkula3@gmail.com>
2024-02-06 19:52:26 +0530

    Included I2C LCD library

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,6 @@
+// Including the libraries
+#include <LiquidCrystal_I2C.h>
+
 // Pin definitions
 // Pin definitions should be added here and any references to the pins should be done through these definitions and should never be made directly to the pin address unless absolutely necessary.
 #define error_pin 13
