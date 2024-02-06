2bd02cd736a2444fa0651b2b851e719e4ea9ed95
RandithaK <bharkula3@gmail.com>
2024-02-06 19:25:34 +0530

    Added function prototypes

--- main.ino
+++ main.ino
@@ -2,6 +2,15 @@
 // Pin definitions should be added here and any references to the pins should be done through these definitions and should never be made directly to the pin address unless absolutely necessary.
 #define error_pin 13
 
+
+
+
+//Function Prototypes
+void get_keypad_input();
+void dispense();
+void error();
+void mode_switch();
+
 void setup() {
   // put your setup code here, to run once:
 
