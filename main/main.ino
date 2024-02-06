bddccf9e585cf836748a1b271ef2798831793147
RandithaK <bharkula3@gmail.com>
2024-02-07 01:42:48 +0530

    Defined dispenser pins

--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,10 @@
 // Pin definitions
 // Pin definitions should be added here and any references to the pins should be done through these definitions and should never be made directly to the pin address unless absolutely necessary.
 #define error_pin 13
+#define dispenser_0 A0
+#define dispenser_1 A1
+#define dispenser_2 A2
+
 
 
 // Setting up devices
