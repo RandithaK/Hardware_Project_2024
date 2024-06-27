bfaa28a86311512ddf7d9ed3b8bebb4d69059693
RandithaK <bharkula3@gmail.com>
2024-06-27 12:55:10 +0530

    Refreshed Code

--- main/main.ino
+++ main/main.ino
@@ -9,30 +9,65 @@
 // Pin definitions
 // Pin definitions should be added here and any references to the pins should be done through these definitions and should never be made directly to the pin address unless absolutely necessary.
 
-#define dispenser_0 A0
-#define dispenser_1 A1
-#define dispenser_2 A2
-#define weight_cell A3
+// Dispensing Control Pins
+#define dispenser_0_Open A0
+#define dispenser_0_Close A1
+#define dispenser_1_Open A2
+#define dispenser_1_Close A3
+#define dispenser_2_Open A4
+#define dispenser_2_Close A5
+
+// Sealing Unit Control
+#define sealer_assembly_Open 52
+#define sealer_assembly_Open 53
+
+
+// Weight Scale
+#define weight_cell_DATA 4
+#define weight_cell_CLK 5
+
 // Pins A4 and A5 are used as communication pins SCL and SDA with the LCD
 // Pins 0 and 1 used for serial communication and hence not used for any other purpose for stabilty
-#define sealer_assembly 2
-#define servo 3
-#define mode_switch 4
-#define keypad_col4 5
-#define keypad_col3 6
-#define keypad_col2 7
-#define keypad_col1 8
-#define keypad_row4 9
-#define keypad_row3 10
-#define keypad_row2 11
-#define keypad_row1 12
-#define error_pin 13
+
+
+
+// Keypad Connections
+#define keypad_col1 30
+#define keypad_col2 32
+#define keypad_col3 34
+#define keypad_col4 36
+
+#define keypad_row1 31
+#define keypad_row2 33
+#define keypad_row3 35
+#define keypad_row4 37
+
+// Buzzer
+#define Buzzer 13
 
 
 // Setting up devices
 LiquidCrystal_I2C lcd(0x27,20,4); // Setting up the LCD at address 0x27
 
 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+
+
+
+
+
+/*
 //Function Prototypes
 void get_keypad_input();
 void dispense();
@@ -93,3 +128,5 @@ void mode_switcher(){
     digitalWrite(mode_switch,HIGH);
   }
 }
+
+*/
