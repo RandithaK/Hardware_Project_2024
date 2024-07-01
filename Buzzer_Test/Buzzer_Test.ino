a908f3f22e1607b3a246e593656fd94feff55c42
RandithaK <bharkula3@gmail.com>
2024-07-01 14:15:03 +0530

    Minor Adjestments

--- Buzzer_Test/Buzzer_Test.ino
+++ Buzzer_Test/Buzzer_Test.ino
@@ -4,17 +4,20 @@ void setup() {
 
    pinMode(buzzer, OUTPUT);
 
+      tone(buzzer,1535,500);
+
+      //add another 500 milliseconds of silence
+
+      delay(1000);
+      noTone(buzzer);
+      digitalWrite(buzzer, HIGH);
+
+
 }
 
  
 void loop() {
 
      //turn on the buzzer at 1535 frequency for 500 milliseconds
-
-     tone(buzzer,1535,500);
-
-     //add another 500 milliseconds of silence
-
-     delay(1000);
 
 }
